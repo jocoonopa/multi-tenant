ddfe61807e10e8b803877cf488c51618abcef63f
jocoonopa <jocoonopa@gmail.com>
2025-05-25 22:10:25 +0800

    Update DatabaseCommand.php

--- tests/unit-tests/Commands/DatabaseCommand.php
+++ tests/unit-tests/Commands/DatabaseCommand.php
@@ -22,7 +22,7 @@ use Illuminate\Contracts\Foundation\Application;
 /**
  * @doesNotPerformAssertions
  */
-abstract class DatabaseCommand extends Test
+class DatabaseCommand extends Test
 {
     use InteractsWithMigrations;
 
